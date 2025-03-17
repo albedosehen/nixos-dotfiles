c65d4a6dcb71d39856677ad9929a07e332184fb8
albedosehen <shonpt@outlook.com>
2025-03-17 18:37:43 +0000

    update git configs

--- modules/git-profile.nix
+++ modules/git-profile.nix
@@ -1,9 +1,11 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+
 let
   sshKeyPathPersonal = "${config.home.homeDirectory}/.ssh/id_ed25519_git";
   sshKeyPathWork = "${config.home.homeDirectory}/.ssh/id_rsa_paradigm";
 in
 {
+  # SSH Key Generation Script
   home.activation.scripts.sshKeySetup = ''
     if [ ! -f ${sshKeyPathPersonal} ]; then
       echo "Generating SSH key for GitHub (personal)"
@@ -11,10 +13,22 @@ in
     fi
     if [ ! -f ${sshKeyPathWork} ]; then
       echo "Generating SSH key for Bitbucket (work)"
-      ssh-keygen -t rsa -b 4096 -C "shon.thomas@myparadign.com" -f ${sshKeyPathWork} -N ""
+      ssh-keygen -t rsa -b 4096 -C "shon.thomas@myparadigm.com" -f ${sshKeyPathWork} -N ""
     fi
   '';
 
+  # Ensure correct file permissions for SSH keys
+  home.file.".ssh/id_ed25519_git" = {
+    source = null; # no need to embed content directly
+    mode = "0600"; # Correct permissions for private key
+  };
+
+  home.file.".ssh/id_rsa_paradigm" = {
+    source = null; # no need to embed content directly
+    mode = "0600"; # Correct permissions for private key
+  };
+
+  # SSH config
   home.file.".ssh/config".text = ''
     Host github.com
       HostName github.com
@@ -26,8 +40,4 @@ in
       User git
       IdentityFile ${sshKeyPathWork}
   '';
-
-  # Ensure correct permissions for the SSH keys
-  home.file.".ssh/id_ed25519_git".mode = "0600";
-  home.file.".ssh/id_rsa_paradigm".mode = "0600";
 }
