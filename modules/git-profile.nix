e84ee8852ef2a18f231a406d5c80fdc6764cfe7b
albedosehen <shonpt@outlook.com>
2025-03-17 18:54:43 +0000

    update

--- modules/git-profile.nix
+++ modules/git-profile.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, ... }:
 
 let
   sshKeyPathPersonal = "${config.home.homeDirectory}/.ssh/id_ed25519_git";
@@ -6,22 +6,18 @@ let
 in
 {
   # SSH Key Generation and File Setup as Activation Script
-  home.activation.scripts = [
-    # Generate SSH keys if they don't exist
-    {
-      name = "generate-ssh-keys";
-      text = ''
-        if [ ! -f ${sshKeyPathPersonal} ]; then
-          echo "Generating SSH key for GitHub (personal)"
-          ssh-keygen -t ed25519 -C "shonpt@outlook.com" -f ${sshKeyPathPersonal} -N ""
-        fi
-        if [ ! -f ${sshKeyPathWork} ]; then
-          echo "Generating SSH key for Bitbucket (work)"
-          ssh-keygen -t rsa -b 4096 -C "shon.thomas@myparadigm.com" -f ${sshKeyPathWork} -N ""
-        fi
-      '';
-    }
-  ];
+  sshActivationAction = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -f ${sshKeyPathPersonal} ]; then
+      echo "Generating SSH key for GitHub (personal)"
+      ssh-keygen -t ed25519 -C "shonpt@outlook.com" -f ${sshKeyPathPersonal} -N ""
+    fi
+    if [ ! -f ${sshKeyPathWork} ]; then
+      echo "Generating SSH key for Bitbucket (work)"
+      ssh-keygen -t rsa -b 4096 -C "shon.thomas@myparadigm.com" -f ${sshKeyPathWork} -N ""
+    fi
+  '';
+
+  home.activation.scripts.sshKeySetup = sshActivationAction;
 
   # File configuration for SSH keys with correct file permissions
   #home.file.".ssh/id_ed25519_git" = {
