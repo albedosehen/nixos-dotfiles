11868df61656f84c75dbc267dd2068b46824e799
albedosehen <shonpt@outlook.com>
2025-03-19 07:45:07 +0000

    fix nix-index

--- home.nix
+++ home.nix
@@ -189,9 +189,9 @@ in
   };
 
   # Add nix-index configuration
-  #programs.nix-index = {
-  #enable = true;
-  #enableZshIntegration = true;
+  programs.nix-index = {
+    enable = true;
+    enableZshIntegration = true;
   #enableFishIntegration = true;
-  #};
+  };
 }
